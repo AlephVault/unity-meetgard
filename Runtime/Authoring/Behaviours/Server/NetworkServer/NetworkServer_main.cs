a498d08d4462359bd4316706841a074617e90387
Luis Masuelli <luis@datagran.io>
2021-11-17 17:48:31 -0500

    Disabling the debug in meetgard

--- Runtime/Authoring/Behaviours/Server/NetworkServer/NetworkServer_main.cs
+++ Runtime/Authoring/Behaviours/Server/NetworkServer/NetworkServer_main.cs
@@ -24,7 +24,7 @@ namespace AlephVault.Unity.Meetgard
                 public partial class NetworkServer : MonoBehaviour
                 {
                     // Whether to debug or not using XDebug.
-                    private static bool debug = true;
+                    private static bool debug = false;
 
                     // The current listener.
                     private TcpListener listener = null;
