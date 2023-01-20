faec522749de877a9d9a02946700bde40cf81b7c
Luis Masuelli <luis@datagran.io>
2023-01-20 14:52:13 -0500

    Fixing the namespaces

--- Runtime/Authoring/Behaviours/Client/MandatoryHandshakeProtocolClientSide.cs
+++ Runtime/Authoring/Behaviours/Client/MandatoryHandshakeProtocolClientSide.cs
@@ -1,56 +1,59 @@
-using AlephVault.Unity.Meetgard.Authoring.Behaviours.Client;
 using System;
 using System.Threading.Tasks;
+using AlephVault.Unity.Meetgard.Protocols;
 using AlephVault.Unity.Support.Utils;
 
 namespace AlephVault.Unity.Meetgard
 {
-    namespace Protocols
+    namespace Authoring
     {
-        namespace Simple
+        namespace Behaviours
         {
-            /// <summary>
-            ///   This is the client-side implementation of a mandatory
-            ///   handshake protocol. In this case, the client-side
-            ///   will have a Welcome and a Timeout event, which are
-            ///   triggered on the respective messages.
-            /// </summary>
-            public class MandatoryHandshakeProtocolClientSide : ProtocolClientSide<MandatoryHandshakeProtocolDefinition>
+            namespace Client
             {
-                protected override void SetIncomingMessageHandlers()
+                /// <summary>
+                ///   This is the client-side implementation of a mandatory
+                ///   handshake protocol. In this case, the client-side
+                ///   will have a Welcome and a Timeout event, which are
+                ///   triggered on the respective messages.
+                /// </summary>
+                public class MandatoryHandshakeProtocolClientSide : ProtocolClientSide<MandatoryHandshakeProtocolDefinition>
                 {
-                    AddIncomingMessageHandler("Welcome", async (proto) =>
+                    protected override void SetIncomingMessageHandlers()
                     {
-                        // The OnWelcome event is triggered. The implementation
-                        // must, as fast as it can, invoke athe expected method
-                        // (i.e. handshake message) in this event handler.
-                        await (OnWelcome?.InvokeAsync() ?? Task.CompletedTask);
-                    });
-                    AddIncomingMessageHandler("Timeout", async (proto) =>
-                    {
-                        // The OnTimeout event is triggered. This is weird to
-                        // occur if the OnWelcome event implements the sending
-                        // of the messages immediately. Nevertheless, the event
-                        // exists because it is triggered. Expect a disconnection
-                        // after this event triggers.
-                        await (OnTimeout?.InvokeAsync() ?? Task.CompletedTask);
-                    });
-                }
-                
-                /// <summary>
-                ///   Triggered when a Welcome message is received. This message
-                ///   is received after immediately connecting: it is the first
-                ///   message received from the server. This message should be
-                ///   listened to send the expected handshake message as fastest
-                ///   as possible.
-                /// </summary>
-                public event Func<Task> OnWelcome = null;
+                        AddIncomingMessageHandler("Welcome", async (proto) =>
+                        {
+                            // The OnWelcome event is triggered. The implementation
+                            // must, as fast as it can, invoke athe expected method
+                            // (i.e. handshake message) in this event handler.
+                            await (OnWelcome?.InvokeAsync() ?? Task.CompletedTask);
+                        });
+                        AddIncomingMessageHandler("Timeout", async (proto) =>
+                        {
+                            // The OnTimeout event is triggered. This is weird to
+                            // occur if the OnWelcome event implements the sending
+                            // of the messages immediately. Nevertheless, the event
+                            // exists because it is triggered. Expect a disconnection
+                            // after this event triggers.
+                            await (OnTimeout?.InvokeAsync() ?? Task.CompletedTask);
+                        });
+                    }
+                    
+                    /// <summary>
+                    ///   Triggered when a Welcome message is received. This message
+                    ///   is received after immediately connecting: it is the first
+                    ///   message received from the server. This message should be
+                    ///   listened to send the expected handshake message as fastest
+                    ///   as possible.
+                    /// </summary>
+                    public event Func<Task> OnWelcome = null;
 
-                /// <summary>
-                ///   Triggered when the server determines the client did not
-                ///   send the handshake after a specified time.
-                /// </summary>
-                public event Func<Task> OnTimeout = null;
+                    /// <summary>
+                    ///   Triggered when the server determines the client did not
+                    ///   send the handshake after a specified time.
+                    /// </summary>
+                    public event Func<Task> OnTimeout = null;
+                }
             }
         }
     }
--- Runtime/Authoring/Behaviours/Server/MandatoryHandshakeProtocolServerSide.cs
+++ Runtime/Authoring/Behaviours/Server/MandatoryHandshakeProtocolServerSide.cs
@@ -1,151 +1,153 @@
-using AlephVault.Unity.Meetgard.Authoring.Behaviours.Server;
 using System;
 using System.Collections;
 using System.Collections.Concurrent;
-using System.Threading;
 using System.Threading.Tasks;
+using AlephVault.Unity.Meetgard.Protocols;
 using UnityEngine;
 
 namespace AlephVault.Unity.Meetgard
 {
-    namespace Protocols
+    namespace Authoring
     {
-        namespace Simple
+        namespace Behaviours
         {
-            /// <summary>
-            ///   This is the server-side implementation of the
-            ///   mandatory handshake protocol. Once a connection
-            ///   is established, it is accounted to be "pending".
-            ///   If the connection remains pending for a long
-            ///   time, it will be Timeout-kicked. The server
-            ///   must determine when a handshake is successfully
-            ///   completed and remove the connection from being
-            ///   handshake-pending, so timeouts do not occur.
-            /// </summary>
-            public class MandatoryHandshakeProtocolServerSide : ProtocolServerSide<MandatoryHandshakeProtocolDefinition>
+            namespace Server
             {
                 /// <summary>
-                ///   The timeout to kick a connection that did
-                ///   not send a handshake message appropriately.
+                ///   This is the server-side implementation of the
+                ///   mandatory handshake protocol. Once a connection
+                ///   is established, it is accounted to be "pending".
+                ///   If the connection remains pending for a long
+                ///   time, it will be Timeout-kicked. The server
+                ///   must determine when a handshake is successfully
+                ///   completed and remove the connection from being
+                ///   handshake-pending, so timeouts do not occur.
                 /// </summary>
-                [SerializeField]
-                private float handshakeTimeout = 5f;
-
-                // This tracks the handshake-pending connections.
-                private Coroutine handshakeTimeoutCoroutine;
-
-                // This is a dict that will be used to track
-                // the timeout of handshake pending connections.
-                private ConcurrentDictionary<ulong, float> pendingHandshakeConnections = new ConcurrentDictionary<ulong, float>();
-
-                /// <summary>
-                ///   This is a sender for the Welcome message.
-                /// </summary>
-                private Func<ulong, Task> SendWelcome;
-
-                /// <summary>
-                ///   This is a sender for the Timeout message.
-                /// </summary>
-                private Func<ulong, Task> SendTimeout;
-                
-                /// <summary>
-                ///   Typically, in this Start callback function
-                ///   all the Send* shortcuts will be instantiated.
-                ///   This time, also the timeout coroutine is
-                ///   spawned immediately.
-                /// </summary>
-                protected override void Initialize()
+                public class MandatoryHandshakeProtocolServerSide : ProtocolServerSide<MandatoryHandshakeProtocolDefinition>
                 {
-                    SendWelcome = MakeSender("Welcome");
-                    SendTimeout = MakeSender("Timeout");
-                    handshakeTimeoutCoroutine = StartCoroutine(HandshakeTimeoutCoroutine());
-                }
+                    /// <summary>
+                    ///   The timeout to kick a connection that did
+                    ///   not send a handshake message appropriately.
+                    /// </summary>
+                    [SerializeField]
+                    private float handshakeTimeout = 5f;
 
-                private void OnDestroy()
-                {
-                    if (handshakeTimeoutCoroutine != null) StopCoroutine(handshakeTimeoutCoroutine);
-                    handshakeTimeoutCoroutine = null;
-                }
+                    // This tracks the handshake-pending connections.
+                    private Coroutine handshakeTimeoutCoroutine;
 
-                // Every second, it updates the handshake-pending timers.
-                private IEnumerator HandshakeTimeoutCoroutine()
-                {
-                    while(true)
+                    // This is a dict that will be used to track
+                    // the timeout of handshake pending connections.
+                    private ConcurrentDictionary<ulong, float> pendingHandshakeConnections = new ConcurrentDictionary<ulong, float>();
+
+                    /// <summary>
+                    ///   This is a sender for the Welcome message.
+                    /// </summary>
+                    private Func<ulong, Task> SendWelcome;
+
+                    /// <summary>
+                    ///   This is a sender for the Timeout message.
+                    /// </summary>
+                    private Func<ulong, Task> SendTimeout;
+                    
+                    /// <summary>
+                    ///   Typically, in this Start callback function
+                    ///   all the Send* shortcuts will be instantiated.
+                    ///   This time, also the timeout coroutine is
+                    ///   spawned immediately.
+                    /// </summary>
+                    protected override void Initialize()
                     {
-                        yield return new WaitForSeconds(1f);
-                        // Yes: it triggers an async function on each frame.
-                        // Checks every 1s that there are no pending connections.
-                        UpdateHandshakePendingTimers(1f);
+                        SendWelcome = MakeSender("Welcome");
+                        SendTimeout = MakeSender("Timeout");
+                        handshakeTimeoutCoroutine = StartCoroutine(HandshakeTimeoutCoroutine());
                     }
-                }
-                
-                /// <summary>
-                ///   Sets up the connection to be handhake pending.
-                ///   Also greets the client.
-                /// </summary>
-                /// <param name="clientId">The just-connected client id</param>
-                public override async Task OnConnected(ulong clientId)
-                {
-                    AddHandshakePending(clientId);
-                    _ = SendWelcome(clientId);
-                }
 
-                /// <summary>
-                ///   Removes the connection from pending handshake
-                ///   and also removes the session, if any. Only one
-                ///   of them will, in practice, be executed.
-                /// </summary>
-                /// <param name="clientId">The just-disconnected client id</param>
-                /// <param name="reason">The exception which is the disconnection reason, if abrupt</param>
-                public override async Task OnDisconnected(ulong clientId, Exception reason)
-                {
-                    RemoveHandshakePending(clientId);
-                }
-                
-                /// <summary>
-                ///   Adds a connection to the pool of pending connections.
-                /// </summary>
-                /// <param name="connection">The connection to add</param>
-                /// <returns>Whether the connection was added or already existed</returns>
-                private bool AddHandshakePending(ulong connection)
-                {
-                    return pendingHandshakeConnections.TryAdd(connection, 0);
-                }
-
-                /// <summary>
-                ///   Removes a connection from the pool of pending connections.
-                /// </summary>
-                /// <param name="connection">The connection to remove</param>
-                /// <returns>Whether the connection was removed or was never there</returns>
-                public bool RemoveHandshakePending(ulong connection)
-                {
-                    return pendingHandshakeConnections.TryRemove(connection, out _);
-                }
-
-                /// <summary>
-                ///   Tells whether a connection is pending.
-                /// </summary>
-                /// <param name="connection">The connection to test</param>
-                /// <returns>Whether the connection is pending or not</returns>
-                public bool IsConnectionHandshakePending(ulong connection)
-                {
-                    return pendingHandshakeConnections.ContainsKey(connection);
-                }
-
-                // Updates all of the pending connections.
-                private async void UpdateHandshakePendingTimers(float delta)
-                {
-                    await Exclusive(async () =>
+                    private void OnDestroy()
                     {
-                        foreach (var pair in pendingHandshakeConnections.ToArray())
+                        if (handshakeTimeoutCoroutine != null) StopCoroutine(handshakeTimeoutCoroutine);
+                        handshakeTimeoutCoroutine = null;
+                    }
+
+                    // Every second, it updates the handshake-pending timers.
+                    private IEnumerator HandshakeTimeoutCoroutine()
+                    {
+                        while(true)
                         {
-                            pendingHandshakeConnections.TryUpdate(pair.Key, pair.Value + delta, pair.Value);
-                            if (pendingHandshakeConnections.TryGetValue(pair.Key, out float value) && value >= handshakeTimeout)
-                            {
-                                _ = SendTimeout(pair.Key);
-                            }
+                            yield return new WaitForSeconds(1f);
+                            // Yes: it triggers an async function on each frame.
+                            // Checks every 1s that there are no pending connections.
+                            UpdateHandshakePendingTimers(1f);
                         }
-                    });
+                    }
+                    
+                    /// <summary>
+                    ///   Sets up the connection to be handhake pending.
+                    ///   Also greets the client.
+                    /// </summary>
+                    /// <param name="clientId">The just-connected client id</param>
+                    public override async Task OnConnected(ulong clientId)
+                    {
+                        AddHandshakePending(clientId);
+                        _ = SendWelcome(clientId);
+                    }
+
+                    /// <summary>
+                    ///   Removes the connection from pending handshake
+                    ///   and also removes the session, if any. Only one
+                    ///   of them will, in practice, be executed.
+                    /// </summary>
+                    /// <param name="clientId">The just-disconnected client id</param>
+                    /// <param name="reason">The exception which is the disconnection reason, if abrupt</param>
+                    public override async Task OnDisconnected(ulong clientId, Exception reason)
+                    {
+                        RemoveHandshakePending(clientId);
+                    }
+                    
+                    /// <summary>
+                    ///   Adds a connection to the pool of pending connections.
+                    /// </summary>
+                    /// <param name="connection">The connection to add</param>
+                    /// <returns>Whether the connection was added or already existed</returns>
+                    private bool AddHandshakePending(ulong connection)
+                    {
+                        return pendingHandshakeConnections.TryAdd(connection, 0);
+                    }
+
+                    /// <summary>
+                    ///   Removes a connection from the pool of pending connections.
+                    /// </summary>
+                    /// <param name="connection">The connection to remove</param>
+                    /// <returns>Whether the connection was removed or was never there</returns>
+                    public bool RemoveHandshakePending(ulong connection)
+                    {
+                        return pendingHandshakeConnections.TryRemove(connection, out _);
+                    }
+
+                    /// <summary>
+                    ///   Tells whether a connection is pending.
+                    /// </summary>
+                    /// <param name="connection">The connection to test</param>
+                    /// <returns>Whether the connection is pending or not</returns>
+                    public bool IsConnectionHandshakePending(ulong connection)
+                    {
+                        return pendingHandshakeConnections.ContainsKey(connection);
+                    }
+
+                    // Updates all of the pending connections.
+                    private async void UpdateHandshakePendingTimers(float delta)
+                    {
+                        await Exclusive(async () =>
+                        {
+                            foreach (var pair in pendingHandshakeConnections.ToArray())
+                            {
+                                pendingHandshakeConnections.TryUpdate(pair.Key, pair.Value + delta, pair.Value);
+                                if (pendingHandshakeConnections.TryGetValue(pair.Key, out float value) && value >= handshakeTimeout)
+                                {
+                                    _ = SendTimeout(pair.Key);
+                                }
+                            }
+                        });
+                    }
                 }
             }
         }
