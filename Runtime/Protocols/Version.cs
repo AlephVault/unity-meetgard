c07f96465911f5016e68a4d9b60e88f3252a4a39
Luis Masuelli <luisfmasuelli@gmail.com>
2024-10-01 20:51:51 -0500

    Modifying the check against versions, and also adding an enum for the release type.

--- Runtime/Protocols/Version.cs
+++ Runtime/Protocols/Version.cs
@@ -14,16 +14,15 @@ namespace AlephVault.Unity.Meetgard
         [Serializable]
         public class Version : ISerializable
         {
-            public const byte Stable = 0;
-            public const byte RC = 1;
-            public const byte Beta = 2;
-            public const byte Alpha = 3;
-            public const byte Prealpha = 4;
+            public enum VersionReleaseType
+            {
+                Stable, RC, Beta, Alpha, PreAlpha
+            }
 
             public byte Major;
             public byte Minor;
             public byte Revision;
-            public byte ReleaseType;
+            public VersionReleaseType ReleaseType;
 
             public Version() {}
 
@@ -37,7 +36,7 @@ namespace AlephVault.Unity.Meetgard
 
             public bool Equals(Version other)
             {
-                return Major == other.Major && Minor == other.Minor && Revision == other.Revision && ReleaseType == other.ReleaseType;
+                return Major == other.Major && Minor == other.Minor;
             }
         }
     }
